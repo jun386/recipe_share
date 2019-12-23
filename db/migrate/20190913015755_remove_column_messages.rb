75202b7417b6f2e0239ebda9d097ca1f0e22df9e
EC2 Default User <ec2-user@ip-172-31-37-78.ap-northeast-1.compute.internal>
2019-12-23 11:20:07 +0000

    six

--- db/migrate/20190913015755_remove_column_messages.rb
+++ db/migrate/20190913015755_remove_column_messages.rb
@@ -1,5 +1,5 @@
 class RemoveColumnMessages < ActiveRecord::Migration[5.0]
   def change
-    remove_column :messages, :receiver_id, :integer
+    # remove_column :messages, :receiver_id, :integer
   end
 end
