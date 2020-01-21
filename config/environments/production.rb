ce518ee7cb822cf6546e79b07412dc8a9b287e51
EC2 Default User <ec2-user@ip-172-31-37-78.ap-northeast-1.compute.internal>
2020-01-21 02:11:02 +0000

    addhost

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { protocol: 'https' , host: 'https://nameless-tundra-54946.herokuapp.com/' }
   
   config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.delivery_method = :smtp 
