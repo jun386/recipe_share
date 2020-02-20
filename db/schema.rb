0415449421311c6ea4a1fc294096af86fa387e42
EC2 Default User <ec2-user@ip-172-31-37-78.ap-northeast-1.compute.internal>
2020-02-20 06:04:46 +0000

    changemessages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200120071930) do
+ActiveRecord::Schema.define(version: 20200220060108) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "inquiry_id"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20200120071930) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "room_id"
+    t.integer  "user_id"
   end
 
   create_table "prefectures", force: :cascade do |t|
