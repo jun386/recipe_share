b89c812aa539d4127551bd13160861ec0d39083b
jun386 <aka6116.aka@gmail.com>
2023-02-26 11:08:39 +0900

    :white_check_mark: relationshipモデルのテストコード追加

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,4 +6,22 @@ FactoryBot.define do
     password_confirmation { 'password' }
     confirmed_at {Time.now - 100}
   end
+
+  factory :user1, :class => 'User' do
+    id { 1 }
+    nickname { Faker::Name.name } 
+    email { Faker::Internet.email }
+    password { 'password' }
+    password_confirmation { 'password' }
+    confirmed_at {Time.now - 100}
+  end
+
+  factory :user2, :class => 'User' do
+    id { 2 }
+    nickname { Faker::Name.name } 
+    email { Faker::Internet.email }
+    password { 'password' }
+    password_confirmation { 'password' }
+    confirmed_at {Time.now - 100}
+  end
 end
