847ff3d435fdf88572be86971c0304efa4c71018
jun386 <aka6116.aka@gmail.com>
2023-02-26 17:48:08 +0900

    :art: user_controllerのリファクタリング チャットルーム検索のコードのリファクタリング viewファイルの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,22 +5,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
     @recipes = Recipe.where(user_id: @user.id).order(created_at: :desc)
-    @currentUserEntry=Entry.where(user_id: current_user.id)
-    @userEntry=Entry.where(user_id: @user.id)
-    if @user.id != current_user.id
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
-            @isRoom = true
-            @roomId = cu.room_id
-          end
-        end
-      end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
-      end
+    currentUserEntry = Entry.where(user_id: current_user.id).pluck(:room_id)
+    userEntry = Entry.where(user_id: @user.id).pluck(:room_id)
+    currentUserRoomId = currentUserEntry & userEntry
+    if currentUserRoomId.present?
+      @roomId = currentUserRoomId[0]
+      @isRoom = true
+    else
+      @room = Room.new
+      @entry = Entry.new
     end
   end
 
@@ -46,22 +39,15 @@ class UsersController < ApplicationController
   def likes
     @user = User.find_by(id: params[:id])
     @likes = Like.where(user_id: @user.id).order(created_at: :desc)
-    @currentUserEntry=Entry.where(user_id: current_user.id)
-    @userEntry=Entry.where(user_id: @user.id)
-    if @user.id != current_user.id
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
-            @isRoom = true
-            @roomId = cu.room_id
-          end
-        end
-      end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
-      end
+    currentUserEntry = Entry.where(user_id: current_user.id).pluck(:room_id)
+    userEntry = Entry.where(user_id: @user.id).pluck(:room_id)
+    currentUserRoomId = currentUserEntry & userEntry
+    if currentUserRoomId.present?
+      @roomId = currentUserRoomId[0]
+      @isRoom = true
+    else
+      @room = Room.new
+      @entry = Entry.new
     end
   end
   
