7b8eb1bce8e859089cda81f57f43a8abee7738fc
jun386 <aka6116.aka@gmail.com>
2023-03-01 08:19:57 +0900

    :art: userモデルのリレーションを修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
     validates :user_id, {presence: true}
     validates :recipe_id, {presence: true}
-    # belongs_to :user
-    # belongs_to :recipe
+    belongs_to :user
+    belongs_to :recipe
 end
