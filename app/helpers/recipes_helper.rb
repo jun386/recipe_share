500e3f8e2cbdf8dd657707d09a8f105fa14d56a8
jun386 <aka6116.aka@gmail.com>
2026-05-01 17:31:39 +0900

    Refactor indentation in RecipesHelper and UsersHelper for improved readability

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,17 +1,17 @@
 module RecipesHelper
-    
-    def text_placeholder
-      <<-"EOS".strip_heredoc
-        １、・・・・・。
-        
-        ２、・・・・・。
-        
-        ３、・・・・・。
-        
-        　　　・
-        　　　・
-        　　　・
-      EOS
-    end
-    
+
+  def text_placeholder
+    <<-"EOS".strip_heredoc
+      １、・・・・・。
+
+      ２、・・・・・。
+
+      ３、・・・・・。
+
+      　　　・
+      　　　・
+      　　　・
+    EOS
+  end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,35 +1,35 @@
 module UsersHelper
-    def current_user?(user)
-     user == current_user
-    end
-    
-    def prefecture_name(id)
-        if id.present?
-            Prefecture.find_by(id: id).name
-        else
-            "設定されていません"
-        end
-    end
-    
-    def gender_name(gender)
-        if gender.present?
-            case gender
-            when "m"
-              "男性"
-            when "f"
-              "女性"
-            when "o"
-              "その他"
-            else
-              "性別が設定されていません"
-            end
-        else
-            "性別が設定されていません"
-        end
-    end
+  def current_user?(user)
+    user == current_user
+  end
 
-    def following_check(user_id)
-      users = User.where(id: current_user.followings & current_user.followers).pluck(:id)
-      users.include?(user_id)
+  def prefecture_name(id)
+    if id.present?
+      Prefecture.find_by(id: id).name
+    else
+      "設定されていません"
     end
+  end
+
+  def gender_name(gender)
+    if gender.present?
+      case gender
+      when "m"
+        "男性"
+      when "f"
+        "女性"
+      when "o"
+        "その他"
+      else
+        "性別が設定されていません"
+      end
+    else
+      "性別が設定されていません"
+    end
+  end
+
+  def following_check(user_id)
+    users = User.where(id: current_user.followings & current_user.followers).pluck(:id)
+    users.include?(user_id)
+  end
 end
